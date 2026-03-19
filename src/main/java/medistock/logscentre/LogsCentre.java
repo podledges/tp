3908cfd77f4d4fff0e5d0a878046e60cddad18dc
Suwei <shresthasuwei@gmail.com>
2026-03-19 15:15:46 +0800

    Add LogsCentre to keep track of logging records under medistock.log file.

---
+++ src/main/java/medistock/logscentre/LogsCentre.java
@@ -0,0 +1,20 @@
+package medistock.logscentre;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LogsCentre {
+    private static final Logger logger = Logger.getLogger(LogsCentre.class.getName());
+
+    public static void initLogging() {
+        try {
+            FileHandler fileHandler = new FileHandler("medistock.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            Logger.getLogger("medistock").addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.warning("Failed to set up file logging: " + e.getMessage());
+        }
+    }
+}
